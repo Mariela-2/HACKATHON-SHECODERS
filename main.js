0a19fa26370935c02826c110aa7ba2bd5b383cd1
Mariela <111813910+Mariela-1@users.noreply.github.com>
2025-02-14 04:27:23 -0500

    register/login

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleForms() {
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
+    loginForm.classList.toggle('d-none');
+    registerForm.classList.toggle('d-none');
+}
