bc99d356cadf0bb2bc1ab66b80ffe223d1b57cfc
Mariela <111813910+Mariela-1@users.noreply.github.com>
2025-02-17 01:37:40 -0500

    register/login

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyB3TdPefbbnlMEHe2StPK4Qhbp--hMYk9E",
+    authDomain: "shecoders-772e7.firebaseapp.com",
+    projectId: "shecoders-772e7",
+    storageBucket: "shecoders-772e7.firebasestorage.app",
+    messagingSenderId: "843179557044",
+    appId: "1:843179557044:web:c06c84c263765cb711d7e1"
+};
+
+export default firebaseConfig;
--- main.js
+++
@@ -1,6 +0,0 @@
-function toggleForms() {
-    const loginForm = document.getElementById('loginForm');
-    const registerForm = document.getElementById('registerForm');
-    loginForm.classList.toggle('d-none');
-    registerForm.classList.toggle('d-none');
-}
